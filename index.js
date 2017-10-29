3f0ebd88566cda080926b6749a5ef5cfe6e77318
Joshua Prince <joshuap127@gmail.com>
2017-10-29 23:25:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var name = "Susan"
 var height = 70
 
 if (name === "Susan") {
-    console.log("The name is correct")
+    //console.log("The name is correct")
   } else{
       console.log("Expected name is Susan")
 
 }
 
 if (height === 70) {
-  console.log("The height is correct")
+  //console.log("The height is correct")
 } else{
     console.log("Expected height is 70")
 }

d0813ea1ce0845113f9cb18252f10410dd98eec1
Joshua Prince <joshuap127@gmail.com>
2017-10-29 22:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
     console.log("The name is correct")
     else{
       console.log("Expected name is Susan")
-    }
+    
 }
 // Don't worry about this
 module.exports = { name, height

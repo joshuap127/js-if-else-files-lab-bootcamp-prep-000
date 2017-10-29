325e58f07fcdb2c3fca64c3e4a05063b2498ba4b
Joshua Prince <joshuap127@gmail.com>
2017-10-29 23:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var name = "Susan"
-var height = 70
+var name = "Joe"
+var height = 74
 
 if (name === "Susan") {
-    //console.log("The name is correct")
+    console.log("The name is correct")
   } else{
       console.log("Expected name is Susan")
 
 }
 
 if (height === 70) {
-  //console.log("The height is correct")
+  console.log("The height is correct")
 } else{
     console.log("Expected height is 70")
 }
